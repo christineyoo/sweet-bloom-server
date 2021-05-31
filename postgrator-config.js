9fa44d2753be0d0136c094ae1c1968f5c08f3a05
Christine Yoo <christinecho.s@gmail.com>
2021-05-31 16:47:52 -0700

    added postgrator-config file

---
+++ postgrator-config.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+module.exports = {
+  migrationsDirectory: 'migrations',
+  driver: 'pg',
+  connectionString:
+    process.env.NODE_ENV === 'development'
+      ? process.env.DATABASE_URL
+      : process.env.TEST_DATABASE_URL
+};
--- src/config.js
+++ src/config.js
@@ -1,4 +1,13 @@
 module.exports = {
-    PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  DATABASE_URL:
+    process.env.DATABASE_URL || 'postgresql://chris@localhost/sweetbloom',
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    'postgresql://chris@localhost/sweetbloom-test',
+  CLIENT_ORIGIN: {
+    origin: 'http://localhost:3000',
+    methods: 'GET'
   }
+};
