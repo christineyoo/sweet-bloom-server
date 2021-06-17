a5b6fa4be5e7ea2baa3ab0c74ac5431130bb403b
Christine Yoo <christinecho.s@gmail.com>
2021-06-17 14:01:01 -0700

    changed test db url

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ module.exports = {
     process.env.DATABASE_URL || 'postgresql://chris@localhost/sweetbloom',
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
-    'postgresql://chris@localhost/sweetbloom-test',
+    'postgresql://chris@localhost/sweetbloom_test',
   CLIENT_ORIGIN: {
     origin: 'http://localhost:3000',
     methods: 'GET'
